de24b067e12a3aa92661b0784c786a6a607412eb
elderluan21 <elderluan.m@gmail.com>
2022-09-24 17:28:55 -0400

    exercicio imc

---
+++ js/codigo.js
@@ -0,0 +1,8 @@
+function imc(){
+    var altura =document.querySelector("#altura").value
+    var peso = document.querySelector("#peso").value 
+    var imc = peso / (altura*altura)
+    if(imc < 18.5){
+        alert('voce está com magreza')
+    }elseif(<imc){}
+}
